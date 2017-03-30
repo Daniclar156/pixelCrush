954328d026af123edeaf1165d25403dacbc62017
daniclar156 <daniclar156@ed.amdsb.ca>
2017-03-30 09:49:44 -0400

    added begining of grid contruction.

--- Pixel Crush/GameScreen.cs
+++ Pixel Crush/GameScreen.cs
@@ -15,11 +15,17 @@ namespace Pixel_Crush
         //variables
         int mouseX, mouseY, score;
 
+        //randomizer
+        Random randPos = new Random();
+
         //brush
         SolidBrush pixelBrush;
 
         //box list
         public static List<Pixel> pixels = new List<Pixel>();
+
+        //playing grid
+       // public static List<int> pGrid = new List<int>(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
 
         public GameScreen()
         {
@@ -29,12 +35,24 @@ namespace Pixel_Crush
 
         public void OnStart()
         {
-            Pixel p = new Pixel(300, 100);
-
-            pixels.Add(p);
+            generateGrid();
             //randomize grid 1 to amount desired cubes, then have a grid corisponding to a set about of spaces. check if match, then set x, y to that grid spot
             //todo figure out how to randomly generate boxes in a grid with pixel spacing of 5
+        }
 
+        public void generateGrid()
+        {
+            foreach (Pixel p in pixels)
+            {
+                int boxPos = 1;//randPos.Next(1, 13);
+                switch (boxPos)
+                {
+                    case 1:
+                        Pixel pp = new Pixel(100, 100);
+                        pixels.Add(pp);
+                        break;
+                }
+            }
         }
 
 
